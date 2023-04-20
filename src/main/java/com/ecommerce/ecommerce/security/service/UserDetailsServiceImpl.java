0c73aa5f9cf1e741c0f2af246b19511eb60c0667
Sendy Joan K <69149460+sendyjoan@users.noreply.github.com>
2023-04-20 12:08:14 +0700

    Auth Service Created

---
+++ src/main/java/com/ecommerce/ecommerce/security/service/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.ecommerce.ecommerce.security.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.ecommerce.ecommerce.entity.Pengguna;
+import com.ecommerce.ecommerce.repository.PenggunaRepository;
+
+public class UserDetailsServiceImpl implements UserDetailsService{
+
+    @Autowired
+    PenggunaRepository penggunaRepository;
+    
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Pengguna pengguna = penggunaRepository.findById(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username " + username + " tidak ditemukan"));
+        return UserDetailsImpl.build(pengguna);
+    }
+    
+}
