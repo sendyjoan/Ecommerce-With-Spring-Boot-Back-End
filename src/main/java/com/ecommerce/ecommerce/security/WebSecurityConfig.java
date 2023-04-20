84ac19ceadf733fbfb3589e1c2bdf6c54754f65e
Sendy Joan K <69149460+sendyjoan@users.noreply.github.com>
2023-04-20 20:19:15 +0700

    Update WebSecurityConfig

--- src/main/java/com/ecommerce/ecommerce/security/WebSecurityConfig.java
+++ src/main/java/com/ecommerce/ecommerce/security/WebSecurityConfig.java
@@ -13,6 +13,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import com.ecommerce.ecommerce.security.jwt.AuthEntryPointJwt;
 import com.ecommerce.ecommerce.security.jwt.AuthTokenFilter;
